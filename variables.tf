626838d1bbe68a49657e983e2693a8e04ac954f8
Brad Sickles <brad.sickles@gmail.com>
2026-05-20 17:11:21 -0400

    Added var.post_app_metadata

--- variables.tf
+++ variables.tf
@@ -7,3 +7,14 @@ EOF
   type    = map(string)
   default = {}
 }
+
+variable "post_app_metadata" {
+  description = <<EOF
+Nullstone automatically injects metadata from the app module into this module through this variable.
+This variable injects specific metadata from the primary app infrastructure as a way to avoid cyclical dependencies.
+This variable is a reserved variable for capabilities.
+EOF
+
+  type    = map(string)
+  default = {}
+}
